9e5e9c172afcf1733d7fa00758dbf01f656ce474
Akhmad Yusuf <afpentech@gmail.com>
2025-09-17 15:57:28 +0800

    dummy data

--- app/Http/Controllers/AsetController.php
+++ app/Http/Controllers/AsetController.php
@@ -3,13 +3,70 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Support\Collection;
 
 class AsetController extends Controller
 {
     public function index()
     {
-        return view('aset.aset');
+        // Data dummy aset
+        $dummyData = [
+            ['nama' => 'Meja Bundar', 'tipe' => 'Furnitur'],
+            ['nama' => 'Kursi Kayu', 'tipe' => 'Furnitur'],
+            ['nama' => 'Lampu Gantung', 'tipe' => 'Elektronik'],
+            ['nama' => 'Lukisan Dinding', 'tipe' => 'Dekorasi'],
+            ['nama' => 'Rak Besi', 'tipe' => 'Furnitur'],
+            ['nama' => 'Speaker Bluetooth', 'tipe' => 'Elektronik'],
+            ['nama' => 'Jam Dinding', 'tipe' => 'Dekorasi'],
+            ['nama' => 'Vas Bunga', 'tipe' => 'Dekorasi'],
+            ['nama' => 'Proyektor', 'tipe' => 'Elektronik'],
+            ['nama' => 'Papan Tulis', 'tipe' => 'Furnitur'],
+        ];
+
+        // Tambahkan kode aset
+        $data = collect($dummyData)->map(function ($item) {
+            $kode = $this->generateKode($item['tipe'], $item['nama']);
+            return [
+                'nama' => $item['nama'],
+                'tipe' => $item['tipe'],
+                'kode' => $kode,
+            ];
+        });
+
+        // Pagination manual (5 per halaman)
+        $perPage = 5;
+        $currentPage = request()->input('page', 1);
+        $pagedData = new LengthAwarePaginator(
+            $data->forPage($currentPage, $perPage),
+            $data->count(),
+            $perPage,
+            $currentPage,
+            ['path' => url()->current()]
+        );
+
+        return view('aset.aset', [
+            'aset' => $pagedData
+        ]);
     }
+
+    private function generateKode($tipe, $nama)
+    {
+        // A = huruf pertama dari tipe aset
+        $kodeTipe = strtoupper(substr($tipe, 0, 1));
+
+        // B = 2 huruf pertama nama aset
+        $words = explode(' ', $nama);
+
+        if (count($words) == 1) {
+            $kodeNama = strtoupper(substr($words[0], 0, 2));
+        } else {
+            $kodeNama = strtoupper(substr($words[0], 0, 1) . substr($words[1], 0, 1));
+        }
+
+        return $kodeTipe . '-' . $kodeNama;
+    }
+
 
     public function create()
     {
