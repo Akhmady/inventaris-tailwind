bc3f9122715c643e9c734dd63a639f99d23fd361
Akhmad Yusuf <afpentech@gmail.com>
2025-09-24 22:37:07 +0800

    migrator

---
+++ database/migrations/2025_09_23_065103_create_aset_ruangans_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('aset_ruangans', function (Blueprint $table) {
+            $table->id();
+            $table->integer('jumlah');
+            $table->enum('kondisi',['Baik', 'Rusak Ringan', 'Rusak Berat']);
+            $table->string('kodeAset');
+            $table->foreignId('ruangan_id')->references('id')->on('ruangans');
+            $table->foreignId('aset_id')->references('id')->on('asets');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('aset_ruangans');
+    }
+};
