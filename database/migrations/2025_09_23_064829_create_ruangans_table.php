cbd9a9979184f1b9658c4227b7cba6205df13692
Akhmad Yusuf <afpentech@gmail.com>
2025-09-30 14:19:20 +0800

    set deskripsi to nullable ( no migration needed )

--- database/migrations/2025_09_23_064829_create_ruangans_table.php
+++ database/migrations/2025_09_23_064829_create_ruangans_table.php
@@ -14,8 +14,9 @@ return new class extends Migration
         Schema::create('ruangans', function (Blueprint $table) {
             $table->id();
             $table->string('nama_ruangan');
-            $table->text('deskripsi_ruangan');
+            $table->text('deskripsi_ruangan')->nullable();
             $table->string('penanggung_jawab');
+            
             
             $table->timestamps();
         });
