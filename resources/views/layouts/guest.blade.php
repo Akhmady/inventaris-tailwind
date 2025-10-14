3f49e3121eac4ffbdcfcf2af94654ffc8591fc21
Akhmad Yusuf <afpentech@gmail.com>
2025-10-14 18:20:26 +0800

    login ui

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,35 +1,42 @@
-
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans text-gray-900 antialiased transition-colors duration-500">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
-            <img src="{{ asset('assets/icons/logo.svg') }}" id="themeLogo"
-            alt="Logo"
-            class="cursor-pointer w-24 h-24 max-w-full flex-shrink-0 dark:filter dark:brightness-0 dark:invert mb-3">
-            <div class="flex items-center justify-start mb-3 transition-all duration-300">
-           
-           <div class="menu-text ml-3 text-md text-3xl font-bold text-gray-800 dark:text-gray-200">
-               Welcome
-           </div>
-       </div>
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
-                {{ $slot }}
-            </div>
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body class="font-sans text-gray-900 antialiased transition-colors duration-500">
+    <div
+        class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 
+                    bg-gradient-to-br from-gray-100 via-gray-200 to-gray-300 dark:from-gray-900 dark:via-gray-800 dark:to-gray-900">
+
+        <!-- Logo -->
+        <img src="{{ asset('assets/icons/logo.svg') }}" id="themeLogo" alt="Logo"
+            class="cursor-pointer w-16 h-16 max-w-full flex-shrink-0 dark:filter dark:brightness-0 dark:invert mb-3">
+
+        <!-- Welcome text -->
+        <div class="flex flex-col items-center mb-6 transition-all duration-300">
+            <h1 class="text-xl font-bold text-gray-800 dark:text-gray-200">Welcome</h1>
+            <p class="text-sm text-gray-600 dark:text-gray-400">Silakan login untuk melanjutkan</p>
         </div>
-    </body>
+
+        <!-- Card -->
+        <div
+            class="w-full sm:max-w-lg mt-6 px-6 py-4 bg-white dark:border-gray-300 dark:bg-gray-800 shadow-lg overflow-hidden sm:rounded-3xl">
+            {{ $slot }}
+        </div>
+    </div>
+</body>
+
 </html>
