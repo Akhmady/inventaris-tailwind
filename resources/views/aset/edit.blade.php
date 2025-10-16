580ecf0662600c22a10787d724273fb03b6c8c20
Akhmad Yusuf <afpentech@gmail.com>
2025-10-16 11:04:19 +0800

    edit aset master

--- resources/views/aset/edit.blade.php
+++ resources/views/aset/edit.blade.php
@@ -2,9 +2,12 @@
 <html lang="id">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Edit Aset</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
