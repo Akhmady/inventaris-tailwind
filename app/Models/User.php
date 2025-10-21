628433fee70018546846850f95e8b8a4b804963a
Akhmad Yusuf <afpentech@gmail.com>
2025-10-21 15:17:23 +0800

    Enable Logging on Admin behavior

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+    public function activityLogs()
+{
+    return $this->hasMany(ActivityLog::class);
+}
+
 }
